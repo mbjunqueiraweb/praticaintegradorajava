34d4ab7cd8c1b43ae675dd0ec846bc19e226dd47
Iberê Abondanza Kuhlmann <ibere.kuhlmann@mercadolivre.com>
2021-12-20 11:18:01 -0300

    attualizando exercicio 5 da folha 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,18 @@
 package com.company;
+import java.util.Arrays;
 import java.util.Scanner;
 // Pratica integradora grupo 11
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Digite aqui seu numero: ");
         Scanner scan;
+
+        //System.out.println("Digite aqui seu numero: ");
+
+
         scan = new Scanner(System.in);
+        /**
         int numeroInteiro = scan.nextInt();
         int numeroPar = 0;
         for (int i = 0; i < numeroInteiro; i += 1) {
@@ -24,7 +29,78 @@ public class Main {
             System.out.println(multiplos);
             multiplos += m;
         }
+
+
+        System.out.println("Digite um número primo:");
+        int numeroPrimo = scan.nextInt();
+
+        if(ehPrimo(numeroPrimo)){
+            System.out.println(numeroPrimo + "é primo.");
+        }else{
+            System.out.println(numeroPrimo + " não é primo");
+        }
+
+        System.out.println("Digite um número:");
+        int m = scan.nextInt();
+
+        for(int i = 2;i <= m; i+=1){
+            if(ehPrimo(i)){
+                System.out.println(i + " é primo");
+            }
+        }
+    */
+
+        System.out.println("Digite n:");
+        int n = scan.nextInt();
+
+        System.out.println("Digite m:");
+        int m = scan.nextInt();
+
+        System.out.println("Digite d:");
+        int d = scan.nextInt();
+
+        int countN = 0;
+        int testando = 0;
+
+        while (countN < n){
+            //System.out.println(ocorrencias(testando, d));
+            if(ocorrencias(testando, d) >= m){
+                System.out.println(testando);
+                countN += 1;
+            }
+            testando+= 1;
+        }
+        System.out.println("fim");
+
+
     }
+
+    public static int ocorrencias ( Integer num, int algar){
+
+        char[] charArr = num.toString().toCharArray();
+
+        int contador = 0;
+        for (char c: charArr){
+           int n = Integer.parseInt(String.valueOf(c));
+            if (n == algar) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static boolean ehPrimo ( int numeroPrimo){
+
+        for (int i = 2; i < numeroPrimo; i+=1){
+            if(numeroPrimo % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
 }
 
 
